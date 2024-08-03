80211fee3806fe52db8cd7466b9cb66cd328acc3
Aadesh Santosh Bhujbal <aadeshbhujbal1@gmail.com>
2024-08-03 11:10:51 +0530

    Live Collabration Added

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -3,28 +3,35 @@
 import { ReactNode } from "react";
 import {
   LiveblocksProvider,
-  RoomProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/ui/Loader";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 
-export function Provider({ children }: { children: ReactNode }) {
+const Provider = ({ children }: { children: ReactNode }) => {
+  const { user: clerkUser } = useUser();
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
-      // publicApiKey={
-      //   "pk_dev_aVxRn9C14LqtfenwrWacUbMMi35pY78TpFLZlCPzCfetfW3NJTupTjCxltN5CFVP"
-      // }
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsers({ userIds });
+
+        return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text,
+        });
+
+        return roomUsers;
+      }}
     >
-      <ClientSideSuspense
-        fallback={
-          <div>
-            <Loader />
-          </div>
-        }
-      >
-        {children}
-      </ClientSideSuspense>
+      <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
   );
-}
+};
+
+export default Provider;
--- lib/actions/room.action.ts
+++ lib/actions/room.action.ts
@@ -3,6 +3,7 @@ import { nanoid } from "nanoid";
 import { liveblocks } from "../liveblock";
 import { revalidatePath } from "next/cache";
 import { parseStringify } from "../utils";
+
 export const createDocument = async ({
   userId,
   email,
@@ -23,11 +24,56 @@ export const createDocument = async ({
     const room = await liveblocks.createRoom(roomId, {
       metadata,
       usersAccesses,
-      defaultAccesses: [],
+      defaultAccesses: ["room:write"],
     });
     revalidatePath("/");
     return parseStringify(room);
   } catch (error) {
     console.log(`Error happened while creating a Room : ${error}`);
+  }
+};
+
+export const getDocument = async ({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+    // if (!hasAccess) {
+    //   throw new Error("You Do Not Have Access To the Document");
+    // }
+    return parseStringify(room);
+  } catch (error) {
+    console.log("Error happened While Getting a ARoom");
+  }
+};
+
+export const updateDocument = async (roomId: string, title: string) => {
+  try {
+    const updatedRoom = await liveblocks.updateRoom(roomId, {
+      metadata: {
+        title,
+      },
+    });
+    revalidatePath(`/documents/${roomId}`);
+    return parseStringify(updatedRoom);
+  } catch (error) {
+    console.log(`Error happened While Updating A Room : ${error}`);
+  }
+};
+
+export const getDocuments = async (email: string) => {
+  try {
+    const rooms = await liveblocks.getRooms({ userId: email });
+
+    return parseStringify(rooms);
+  } catch (error) {
+    console.log("Error happened While Getting a A Rooms");
   }
 };
